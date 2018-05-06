68bd3d0cf1f63226403d12c0b5acd6ce1348d1a3
pradeep.kumar44@gmail.com <pradeep.kumar44@gmail.com>
2018-05-06 11:07:28 +0530

    committing the spread operator.

--- functions.js
+++ functions.js
@@ -27,6 +27,29 @@ var status = result();
 
 console.log(status);
 
+//spread operator
+
+var address={
+    city: "Bangalore",
+    state: "Karnataka"
+}
+
+var customer1 = {name: "Kiran", address: address};
+
+var customer2 = {name: "Praveen", ... address};
+
+console.log(customer1);
+console.log(customer2);
+
+
+var number1 = [1,2,3,4];
+var number2 = [5,6,7,8];
+
+var withoutSpread = [number1, number2];
+
+var withSpread = [...number1, ... number2];
+
+
 
 
    
